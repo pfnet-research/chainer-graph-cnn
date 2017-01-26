64508a2c3a8d869f5f5db3b9b1781ae3b277dda8
Tommi Kerola <tommi@preferred.jp>
2017-01-26 18:49:45 +0900

    Implement GPU graph pooling

--- lib/functions/pooling/graph_max_pooling.py
+++ lib/functions/pooling/graph_max_pooling.py
@@ -3,12 +3,55 @@
 
 import numpy as np
 
+import chainer
 from chainer import function
+from chainer import cuda
+from chainer.cuda import cupy
+
+if chainer.cuda.available:
+    # x will be flattened in C-order
+    # y will be flattened in C-order
+    gpu_graphpool_fwd = cupy.ElementwiseKernel(
+            'I p, raw I pooling_inds, raw T x',
+            'T y, I max_ind',
+            '''
+            int n_cols = _ind.size() / p;
+            int row_idx = i / n_cols;
+            int col_idx = i % n_cols;
+            T x0 = x[pooling_inds[row_idx * 2 + 0] * n_cols + col_idx];
+            T x1 = x[pooling_inds[row_idx * 2 + 1] * n_cols + col_idx];
+            y = max(x0, x1);
+            max_ind = x0 > x1 ? pooling_inds[row_idx * 2 + 0] : pooling_inds[row_idx * 2 + 1];
+            ''',
+            'gpu_graphpool_fwd'
+            )
+
+    gpu_graphpool_bwd = cupy.ElementwiseKernel(
+            'I p, I q, raw I max_inds, raw T gy',
+            'T gx',
+            '''
+            int n_cols = _ind.size() / p;
+            int row_idx = i / n_cols;
+            int col_idx = i % n_cols;
+            T val = 0;
+            for (int j=0; j < q; j++) {
+                int offset = j * n_cols + col_idx;
+                if (max_inds[offset] == row_idx) {
+                    val = gy[offset];
+                    break;
+                }
+            }
+            gx = val;
+
+            ''',
+            'gpu_graphpool_bwd'
+            )
+
 
 class GraphMaxPoolingFunction(function.Function):
 
     def __init__(self, pooling_inds, use_cudnn=True):
-        self.pooling_inds = np.array(pooling_inds)
+        self.pooling_inds = np.array(pooling_inds).astype(np.int32)
         self.use_cudnn = use_cudnn
 
     def check_type_forward(self, in_types):
@@ -30,8 +73,22 @@ class GraphMaxPoolingFunction(function.Function):
         # max_inds.shape = (n_batch, c, N_coarse)
         return x_pairs.max(axis=3),
 
-    #def forward_gpu(self, inputs):
-    #    pass
+    def forward_gpu(self, inputs):
+        x = inputs[0]
+        xp = cuda.get_array_module(x)
+        n_batch, c, N = x.shape
+        N_coarse = len(self.pooling_inds)
+        # x.shape = (n_batch, c, N)
+        x = x.transpose((2, 1, 0))
+        # x.shape = (N, c, n_batch)
+        y = xp.empty((N_coarse, c, n_batch), dtype=x.dtype)
+        self.max_inds = xp.empty((N_coarse, c, n_batch), dtype=np.int32)
+        pooling_inds = cuda.to_gpu(self.pooling_inds)
+        gpu_graphpool_fwd(N_coarse, pooling_inds, x, y, self.max_inds)
+        y = y.transpose((2, 1, 0))
+        # y.shape = (n_batch, c, N_coarse)
+
+        return y,
 
     def backward_cpu(self, inputs, grad_outputs):
         x = inputs[0]
@@ -45,8 +102,23 @@ class GraphMaxPoolingFunction(function.Function):
         gx = gx.reshape(x.shape)
         return gx,
 
-    #def backward_gpu(self, inputs, grad_outputs):
-    #    pass
+    def backward_gpu(self, inputs, grad_outputs):
+        x = inputs[0]
+        xp = cuda.get_array_module(x)
+        n_batch, c_in, N = x.shape
+        # x.shape = (n_batch, c_in, N)
+        x = x.transpose((2, 1, 0))
+        # x.shape = (N, c, n_batch)
+        gy = grad_outputs[0]
+        N_coarse = gy.shape[2]
+        gy = gy.transpose((2, 1, 0))
+        # gy.shape = (n_batch, c_in, N_coarse)
+        gx = xp.zeros((N, c_in, n_batch), dtype=x.dtype)
+        gpu_graphpool_bwd(N, N_coarse, self.max_inds, gy, gx)
+        gx = gx.transpose((2, 1, 0))
+        # gx.shape = (n_batch, c_in, N)
+        return gx,
+
 
 def graph_max_pooling(x, pooling_inds, use_cudnn=True):
     return GraphMaxPoolingFunction(pooling_inds, use_cudnn)(x)
--- tests/test_graph_max_pooling.py
+++ tests/test_graph_max_pooling.py
@@ -5,15 +5,21 @@ import six
 import unittest
 
 import numpy as np
-import scipy.sparse
 
 import chainer
-# from chainer import testing
+from chainer import cuda
+from chainer import testing
 from chainer import gradient_check
+from chainer.testing import attr
+from chainer.testing import condition
 
 from lib.functions.pooling import graph_max_pooling
 from lib import graph
 
+
+@testing.parameterize(*testing.product({
+    'dtype': [np.float32, np.float64],
+}))
 class TestGraphMaxPooling(unittest.TestCase):
 
     def setUp(self):
@@ -26,26 +32,58 @@ class TestGraphMaxPooling(unittest.TestCase):
             [1, 0, 0, 1],
             [1, 0, 0, 0],
             [0, 1, 0, 0],
-                ]).astype(np.float32)
+                ]).astype(self.dtype)
         self.L = graph.create_laplacian(A)
         self.K = 25
         self.pooling_inds = np.array([[0,1],[2,2],[3,3]])
         N_coarse = len(self.pooling_inds)
-        self.x = np.random.randn(n_batch,c_in,N).astype(np.float32)
-        self.gy = np.random.randn(n_batch,c_in,N_coarse).astype(np.float32)
+        self.x = np.arange(n_batch * c_in * N, dtype=self.dtype).reshape((n_batch, c_in, N))
+        self.gy = np.random.randn(n_batch,c_in,N_coarse).astype(self.dtype)
+        self.check_backward_options = {'eps': 2.0 ** -8}
 
-    def test_backward_cpu(self):
-        x_data = self.x
-        y_grad = self.gy
+    def check_forward(self, x_data):
+        x = chainer.Variable(x_data)
+        func = graph_max_pooling.GraphMaxPoolingFunction(self.pooling_inds)
+        y = func(x)
+        self.assertEqual(y.data.dtype, self.dtype)
+        y_data = cuda.to_cpu(y.data)
+
+        self.assertEqual(self.gy.shape, y_data.shape)
+        n_batch, c_in = x.shape[:2]
+        for k in six.moves.range(n_batch):
+            for c in six.moves.range(c_in):
+                x = self.x[k, c]
+                expect = np.array([x[0:2].max(), x[2], x[3]])
+                testing.assert_allclose(expect, y_data[k, c])
+
+    @condition.retry(3)
+    def test_forward_cpu(self):
+        self.check_forward(self.x)
+
+    @attr.gpu
+    @condition.retry(3)
+    def test_forward_gpu(self):
+        self.check_forward(cuda.to_gpu(self.x))
+
+    def check_backward(self, x_data, y_grad):
 
         func = graph_max_pooling.GraphMaxPoolingFunction(self.pooling_inds)
 
         args = (x_data,)
-        check_backward_options = {'dtype': np.float64}
         gradient_check.check_backward(
                 func, args, y_grad,
-                **check_backward_options
+                **self.check_backward_options
                 )
+
+    @condition.retry(3)
+    def test_backward_cpu(self):
+        self.check_backward(self.x, self.gy)
+
+    @attr.gpu
+    @condition.retry(3)
+    def test_backward_gpu(self):
+        self.check_backward(cuda.to_gpu(self.x), cuda.to_gpu(self.gy))
+
 
 if __name__ == '__main__':
     unittest.main()
